bd8d6059fcfbfbe473e5434d7502e692c23b9624
gerardantoun <gerard.antoun@yahoo.com>
2024-05-20 22:26:59 +0200

    added player selection for top 10

--- js/top_players.js
+++ js/top_players.js
@@ -1,6 +1,38 @@
+// Average stats for all players
+const average = [
+  { axis: "KPR", value: 0.71 },
+  { axis: "Surviving", value: 0.38 },
+  { axis: "ADR", value: 0.76 },
+  { axis: "KAST", value: 0.71 },
+  { axis: "Impact", value: 0.5 },
+  { axis: "Rating", value: 0.5 },
+];
+
 const state = {
-    "selectedYear": "2020",
-    "selectedPlayer": 0,
+  selectedYear: "2020",
+  selectedPlayer: 0,
+  data: null,
+  spiderChart: null,
+  initializeData: (data) => {
+    state.data = data;
+    state.spiderChart = new SpiderChart(
+      dom_spiderChart.id,
+      average,
+      transformRawPlayerStats(data[state.selectedYear][state.selectedPlayer]["stats"])
+    );
+    
+    updatePlayerCards(data, state.selectedYear);
+    updateDisplayedPlayer(data, state.selectedYear, state.selectedPlayer);
+  },
+  updatePlayer: (playerIdx) => {
+    state.selectedPlayer = playerIdx;
+    updateDisplayedPlayer(state.data, state.selectedYear, playerIdx);
+  },
+  updateYear: (year) => {
+    state.selectedYear = year;
+    updatePlayerCards(state.data, year);
+    updateDisplayedPlayer(state.data, year, state.selectedPlayer);
+  }
 };
 
 // DOM Elements
@@ -12,7 +44,9 @@ const dom_2nd = document.getElementById("viz1-notable-team-achievements-2nd");
 const dom_3rd = document.getElementById("viz1-notable-team-achievements-3rd");
 const dom_notableStats = document.getElementById("notable-stats-list");
 const dom_playerProfile = document.getElementById("viz1-player-profile");
-const dom_playerSelectionCarousel = document.getElementById("player-selection-carousel");
+const dom_playerSelectionCarousel = document.getElementById(
+  "player-selection-carousel"
+);
 
 const dom_playerRank = document.getElementById("player-rank");
 const dom_playerName = document.getElementById("player-name");
@@ -21,81 +55,123 @@ const dom_playerCountry = document.getElementById("player-country");
 const dom_playerUsername = document.getElementById("player-username");
 const dom_playerImg = document.getElementById("player-profile-img");
 
-// Average stats for all players
-const average = [
-    { axis: "KPR", value: 0.71 },
-    { axis: "Surviving", value: 0.38 },
-    { axis: "ADR", value: 0.76 },
-    { axis: "KAST", value: 0.71 },
-    { axis: "Impact", value: 0.5 },
-    { axis: "Rating", value: 0.5 },
-]
+
 
 // Initial Data Fetch
-fetch('/data/top_players.json')
-.then(response => response.json())
-.then(data => {
-    // Use the data here
-    const playerData = data[state.selectedYear][state.selectedPlayer]
-
-    const playerStats = transformRawPlayerStats(playerData["stats"])
-    // Instantiate the SpiderChart class
-    const spiderChart = new SpiderChart(dom_spiderChart.id, average, playerStats);
-
+fetch("/data/top_players.json")
+  .then((response) => response.json())
+  .then((data) => {
     // Update the player profile
-    updateDisplayedPlayer(data, state.selectedYear, state.selectedPlayer)
-
-})
-.catch(error => {
+    state.initializeData(data);
+  })
+  .catch((error) => {
     // Handle any errors
     console.error(error);
-});
+  });
 
-const updateDisplayedPlayer = (data, year, playerRank) => {
+const updateDisplayedPlayer = (data, year, playerIdx) => {
+  // Spider Chart
+  state.spiderChart.updateChart(transformRawPlayerStats(data[year][playerIdx]["stats"]))
 
-    updateMvp(data[year][playerRank]["mvp"])
-    updateEvp(data[year][playerRank]["evp"])
-    updateNotableTeamAchievements(
-        data[year][playerRank]["team_achievements_1st"], 
-        data[year][playerRank]["team_achievements_2nd"], 
-        data[year][playerRank]["team_achievements_3rd"]
-    )
-    updateNotableStats(data[year][playerRank]["notable_stats"])
-    updatePlayerInfo(data[year][playerRank])
-}
+  // Player Details
+  updateMvp(data[year][playerIdx]["mvp"]);
+  updateEvp(data[year][playerIdx]["evp"]);
+  updateNotableTeamAchievements(
+    data[year][playerIdx]["team_achievements_1st"],
+    data[year][playerIdx]["team_achievements_2nd"],
+    data[year][playerIdx]["team_achievements_3rd"]
+  );
+  updateNotableStats(data[year][playerIdx]["notable_stats"]);
+  updatePlayerInfo(data[year][playerIdx]);
+};
 
 const updateMvp = (mvpList) => {
-    dom_mvp.innerHTML = mvpList.map(player => `<li class="viz1-awards-list-item">${player}</li>`).join("")
-}
+  dom_mvp.innerHTML = mvpList
+    .map((player) => `<li class="viz1-awards-list-item">${player}</li>`)
+    .join("");
+};
 const updateEvp = (evpList) => {
-    dom_evp.innerHTML = evpList.map(player => `<li class="viz1-awards-list-item">${player}</li>`).join("")
-}
+  dom_evp.innerHTML = evpList
+    .map((player) => `<li class="viz1-awards-list-item">${player}</li>`)
+    .join("");
+};
 const updateNotableTeamAchievements = (first, second, third) => {
-    dom_1st.innerHTML = first.map(ach => `<li class="viz1-notable-team-achievement-list-item">${ach}</li>`).join("")
-    dom_2nd.innerHTML = second.map(ach => `<li class="viz1-notable-team-achievement-list-item">${ach}</li>`).join("")
-    dom_3rd.innerHTML = third.map(ach => `<li class="viz1-notable-team-achievement-list-item">${ach}</li>`).join("")
-}
+  dom_1st.innerHTML = first
+    .map(
+      (ach) => `<li class="viz1-notable-team-achievement-list-item">${ach}</li>`
+    )
+    .join("");
+  dom_2nd.innerHTML = second
+    .map(
+      (ach) => `<li class="viz1-notable-team-achievement-list-item">${ach}</li>`
+    )
+    .join("");
+  dom_3rd.innerHTML = third
+    .map(
+      (ach) => `<li class="viz1-notable-team-achievement-list-item">${ach}</li>`
+    )
+    .join("");
+};
 const updateNotableStats = (stats) => {
-    dom_notableStats.innerHTML = stats.map(stat => `<li class="viz1-notable-stats-list-item">${stat}</li>`).join("")
-}
+  dom_notableStats.innerHTML = stats
+    .map((stat) => `<li class="viz1-notable-stats-list-item">${stat}</li>`)
+    .join("");
+};
 const updatePlayerInfo = (player) => {
-    console.log(player)
-    dom_playerRank.innerHTML = `#${player["rank"]}`
-    dom_playerName.innerHTML = player["name"]
-    dom_playerTeam.innerHTML = player["team"]
-    dom_playerCountry.innerHTML = player["country"] ? player["country"] : ""
-    dom_playerUsername.innerHTML = player["username"]
-    dom_playerImg.src = `images/players/${player["username"]}.png`
-}
+  dom_playerRank.innerHTML = `#${player["rank"]}`;
+  dom_playerName.innerHTML = player["name"];
+  dom_playerTeam.innerHTML = player["team"];
+  dom_playerCountry.innerHTML = player["country"] ? player["country"] : "";
+  dom_playerUsername.innerHTML = player["username"];
+  dom_playerImg.src = `images/players/${player["username"]}.png`;
+};
+
+const updatePlayerCards = (data, year) => {
+  const playersList = data[year];
+
+  playersList.forEach((player) => {
+    // Create an HTML element
+    const playerCard = document.createElement("div");
+    playerCard.classList.add("player-card");
+
+    const playerIdx = player["rank"]-1;
+
+    if (playerIdx === state.selectedPlayer) {
+      playerCard.classList.add("selected-player-card");
+    }
+
+    playerCard.innerHTML = `
+    <img
+      src="images/players/${player["username"]}.png"
+      alt="Player Image"
+      class="player-card-img"
+      id="player-card-img-${player["rank"]}"
+    />
+    <div class="player-card-info">
+      <div id="player-rank-${player["rank"]}">#${player["rank"]}</div>
+      <div id="player-username-${player["rank"]}">${player["username"]}</div>
+    </div>
+  `;  
+
+    playerCard.onclick = () => {
+      const allPlayerCards = document.querySelector(".selected-player-card");
+      allPlayerCards.classList.remove("selected-player-card");
+      playerCard.classList.add("selected-player-card");
+      state.updatePlayer(playerIdx);
+    };
+
+    dom_playerSelectionCarousel.appendChild(playerCard);
+  });
+};
 
 // Helper Functions
 const transformRawPlayerStats = (rawStats) => {
-    return [
-        { axis: "KPR", value: rawStats["kpr"] },
-        { axis: "Surviving", value: rawStats["surviving"] / 100 },
-        { axis: "ADR", value: rawStats["adr"] / 100 },
-        { axis: "KAST", value: rawStats["consistency"] / 100 },
-        { axis: "Impact", value: rawStats["impact"]/2 },
-        { axis: "Rating", value: rawStats["rating"]/2 },
-    ]
-}
+  return [
+    { axis: "KPR", value: rawStats["kpr"] },
+    { axis: "Surviving", value: rawStats["surviving"] / 100 },
+    { axis: "ADR", value: rawStats["adr"] / 100 },
+    { axis: "KAST", value: rawStats["consistency"] / 100 },
+    { axis: "Impact", value: rawStats["impact"] / 2 },
+    { axis: "Rating", value: rawStats["rating"] / 2 },
+  ];
+};

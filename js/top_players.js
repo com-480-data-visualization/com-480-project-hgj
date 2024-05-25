9c5a0277c05bb31f45dcefb4ec1382c48e9beb1b
GerardAntoun <gerard.antoun@yahoo.com>
2024-05-25 17:07:07 +0200

    top players JSON url update

--- js/top_players.js
+++ js/top_players.js
@@ -60,7 +60,7 @@ dom_yearSelection.onchange = (event) => {
 };
 
 // Initial Data Fetch
-fetch("../data/top_players.json")
+fetch("/com-480-project-hgj/data/top_players.json")
   .then((response) => response.json())
   .then((data) => {
     // Update the player profile

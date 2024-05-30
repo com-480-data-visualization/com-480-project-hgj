9ca67107e25c0ade1920349b5b97a9c5ab31d8b7
djx <jiaxin.dong@epfl.ch>
2024-05-30 02:45:24 +0200

    viz3 completed

--- js/maps.js
+++ js/maps.js
@@ -9,9 +9,11 @@ slider.addEventListener("input", function() {
 });
 
 // add teams' names
-fetch("/data/map_statistics.json")
+fetch("/com-480-project-hgj/data/map_statistics.json")
   .then((response) => response.json())
   .then((summary) => {
+
+    allTeams = [...new Set(summary.map(d => d.team))];
 
     const teams = [...new Set(summary.map(d => d.team))];
 
@@ -26,12 +28,12 @@ fetch("/data/map_statistics.json")
     console.error(error);
   });
 
-// setup bubbleplot parameters
-var margin = {top: 40, right: 150, bottom: 60, left: 30},
-    width = 500 - margin.left - margin.right,
-    height = 420 - margin.top - margin.bottom;
+// setup graph parameters
+var margin = {top: 40, right: 250, bottom: 60, left: 30},
+    width = 800 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
+// append the svg object
 var svg = d3.select("#my_dataviz")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -41,32 +43,65 @@ var svg = d3.select("#my_dataviz")
           "translate(" + margin.left + "," + margin.top + ")");
 
 
-fetch("/data/map_statistics.json")
+fetch("/com-480-project-hgj/data/map_statistics.json")
 .then(response => response.json())
 .then((summary) => {
-    // Update visualization based on initial selection
+    // update visualization based on initial selection
+    document.getElementById('team-select').value = '100 Thieves'
     const selectedTeam = document.getElementById('team-select').value;
     const selectedYear = document.getElementById('year-slider').value;
     updateBubblePlot(summary, selectedYear, selectedTeam);
+    flag = 1;
 
-    // Add event listeners
+    // update data when the selected team or selected year changes
     document.getElementById('team-select').addEventListener('change', event => {
+        flag = 1;
         const selectedTeam = event.target.value;
         const selectedYear = document.getElementById('year-slider').value;
         updateBubblePlot(summary, selectedYear, selectedTeam);
+        const select = document.getElementById('search-bar');
+        select.value = '';
     });
     document.getElementById('year-slider').addEventListener('input', event => {
         const selectedYear = event.target.value;
         document.getElementById('selected-year').innerText = selectedYear;
-        const selectedTeam = document.getElementById('team-select').value;
-        updateBubblePlot(summary, selectedYear, selectedTeam);
+        if (flag == 1){
+            const selectedTeam = document.getElementById('team-select').value;
+            updateBubblePlot(summary, selectedYear, selectedTeam);
+        }
+        else{
+            const selectedTeam = document.getElementById('search-bar').value;
+            updateBubblePlot(summary, selectedYear, selectedTeam);
+            // clear the previous choice
+            const select = document.getElementById('team-select');
+            select.innerHTML = '<option value="" disabled selected>Select the team</option>';
+            allTeams.forEach(function(team) {
+                const option = document.createElement("option");
+                option.text = team;
+                select.add(option);
+            });
+        }
     });
+    document.getElementById('search-bar').addEventListener('input', function() {
+        flag = 0;
+        const query = this.value;
+        const selectedYear = document.getElementById('year-slider').value;
+        updateBubblePlot(summary, selectedYear, query);
+        // clear the previous choice
+        const select = document.getElementById('team-select');
+        select.innerHTML = '<option value="" disabled selected>Select the team</option>';
+        allTeams.forEach(function(team) {
+            const option = document.createElement("option");
+            option.text = team;
+            select.add(option);
+        });
+    })
 })
 .catch(error => {
     console.error(error);
 });
 
-//plot Bubble chart
+// function to plot Bubble chart
 
 function updateBubblePlot(data, selectedYear, selectedTeam) {
     svg.selectAll('*').remove();
@@ -77,13 +112,57 @@ function updateBubblePlot(data, selectedYear, selectedTeam) {
 function createBubblePlot(data) {
     const teams = [...new Set(data.map(d => d.team))];
     const maps = [...new Set(data.map(d => d.map))];
+    
+    // add tooltip (invisible by default)
+    var tooltip = d3.select("#my_dataviz")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "black")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+        .style("color", "white")
+    // tooltip will be displayed when the mouse moves to the corresponding bubble 
+    var showTooltip = function(event, d) {
+      tooltip
+        .transition()
+        .duration(200)
+      tooltip
+        .style("opacity", 1)
+        .html("Map: " + d.map)
+        .style("left", (event.pageX+10) + "px")
+        .style("top", (event.pageY+10) + "px")
+        }
+    // tooltip follows the mouse movement
+    var moveTooltip = function(event, d) {
+      tooltip
+        .style("left", (event.pageX+10) + "px")
+        .style("top", (event.pageY+10) + "px")
+        }
+    var hideTooltip = function(d) {
+      tooltip
+        .transition()
+        .duration(200)
+        .style("opacity", 0)
+        }
 
+    var highlight = function(d){
+        d3.selectAll(".bubbles").style("opacity", .1)
+        d3.selectAll("." + d).style("opacity", 1)
+    }
+    
+    var refresh = function(d){
+        d3.selectAll(".bubbles").style("opacity", 1)
+    }
+
+    // add bubble
+    // define x axis
     var x = d3.scaleLinear()
         .domain([0, 1])
         .range([ 0, width ]);
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x).ticks(3));
+        .call(d3.axisBottom(x));
 
     svg.append("text")
         .attr("text-anchor", "end")
@@ -91,6 +170,7 @@ function createBubblePlot(data) {
         .attr("y", height+50 )
         .text("CT win rate");
 
+    // define y axis
     var y = d3.scaleLinear()
         .domain([0, 1])
         .range([ height, 0]);
@@ -103,14 +183,15 @@ function createBubblePlot(data) {
         .attr("y", -20 )
         .text("T win rate")
         .attr("text-anchor", "start")
-
+    
+    // define bubble size and data range    
     var z = d3.scaleSqrt()
-        .domain([0, 90])
-        .range([ 2, 30]);
+        .domain([0, 90]) // data range
+        .range([2, 70]); // bubble size
 
     var myColor = d3.scaleOrdinal()
         .domain(maps)
-        .range(d3.schemeSet1);
+        .range(d3.schemeSet2);
 
     svg.append('g')
         .selectAll("dot")
@@ -122,27 +203,90 @@ function createBubblePlot(data) {
         .attr("cy", function (d) { return y(d.CT_win_rate); } )
         .attr("r", function (d) { return z(d.T_total_matches); } )
         .style("fill", function (d) { return myColor(d.map); } )
+        .on("mouseover", showTooltip)
+        .on("mousemove", moveTooltip )
+        .on("mouseleave", hideTooltip)
 
-  // Add legends
-    var size = 20
-    var allgroups = maps
+    // Add legends
+    var size = 30
+    var labels = maps
+    svg.append("text")
+        .attr('x', 640)
+        .attr("y", 50)
+        .text("Maps")
+        .attr("text-anchor", "middle")
+
     svg.selectAll("myrect")
-        .data(allgroups)
+        .data(labels)
         .enter()
         .append("circle")
-        .attr("cx", 390)
-        .attr("cy", function(d,i){ return 10 + i*(size+5)})
-        .attr("r", 7)
+        .attr("cx", 600)
+        .attr("cy", function(d, i){ return i*(size+5) + 100})
+        .attr("r", 10)
         .style("fill", function(d){ return myColor(d)})
+        .on("mouseover", function(event, d){highlight(d)})
+        .on("mouseleave", refresh)
 
     svg.selectAll("mylabels")
-        .data(allgroups)
+        .data(labels)
         .enter()
         .append("text")
-        .attr("x", 390 + size*.8)
-        .attr("y", function(d,i){ return i * (size + 5) + (size/2)})
+        .attr("x", 600 + size*.8)
+        .attr("y", function(d, i){ return i*(size+5) + 100})
         .style("fill", function(d){ return myColor(d)})
         .text(function(d){ return d})
         .attr("text-anchor", "left")
         .style("alignment-baseline", "middle")
-  }
+        .on("mouseover", function(event, d){highlight(d)})
+        .on("mouseleave", refresh)
+    
+}
+
+
+const mapContainer = document.getElementById('map-container');
+
+// read disliked maps' data
+fetch('data/most_disliked_maps.json')
+.then(response => response.json())
+.then((dismap) => {
+
+    // update visualization based on initial selection
+    const teams = [...new Set(dismap.map(d => d.team))];
+    updateDislikedMaps(dismap, teams[0]);
+
+    // update data when the selected team changes
+    document.getElementById('team-select').addEventListener('change', event => {
+        const selectedTeam = event.target.value;
+        updateDislikedMaps(dismap, selectedTeam);
+    });
+
+    document.getElementById('search-bar').addEventListener('input', function() {
+        const query = this.value;
+        updateDislikedMaps(dismap, query);
+    });
+})
+.catch(error => {
+    console.error('Error loading the JSON data:', error);
+});
+
+// function to show most disliked maps of the select team
+function updateDislikedMaps(data, selectedTeam) {
+    // clear previous output
+    mapContainer.innerHTML = ''; 
+
+    const teamData = data.filter(d => d.team === selectedTeam);
+    teamData.forEach(item => {
+        const mapDiv = document.createElement('div');
+        mapDiv.className = 'map';
+
+        const mapName = document.createElement('p');
+        mapName.textContent = `${item.map} (Banned ${item.ban_count} times)`;
+
+        const mapImage = document.createElement('img');
+        mapImage.src = `images/maps/${item.map}.jpg`; 
+
+        mapDiv.appendChild(mapName);
+        mapDiv.appendChild(mapImage);
+        mapContainer.appendChild(mapDiv);
+    });
+}
